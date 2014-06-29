ad21e50d9df8edb605f3f1143c47d503c6936aa2
Frank Woeckener <fwoeck@gmail.com>
2014-06-29 08:22:22 +0200

    receive amqp messages for voice.rails, send to voice.push

--- app/models/amqp_manager.rb
+++ app/models/amqp_manager.rb
@@ -1,25 +1,41 @@
 module AmqpManager
   class << self
 
-    def channel
-      Thread.current[:channel] ||= @connection.create_channel
+
+    def rails_channel
+      Thread.current[:rails_channel] ||= @connection.create_channel
     end
 
-    def xchange
-      Thread.current[:xchange] ||= channel.topic('voice.rails', auto_delete: false)
+
+    def rails_xchange
+      Thread.current[:rails_xchange] ||= rails_channel.topic('voice.rails', auto_delete: false)
     end
 
-    def queue
-      Thread.current[:queue] ||= channel.queue('voice.rails', auto_delete: false)
+
+    def rails_queue
+      Thread.current[:rails_queue] ||= rails_channel.queue('voice.rails', auto_delete: false)
     end
 
-    def rails_publish(*args)
-      xchange.publish(*args)
+
+    def push_channel
+      Thread.current[:push_channel] ||= @connection.create_channel
     end
+
+
+    def push_xchange
+      Thread.current[:push_xchange] ||= push_channel.fanout('voice.push', auto_delete: false)
+    end
+
+
+    def publish_to_push(*args)
+      push_xchange.publish(*args)
+    end
+
 
     def shutdown
       @connection.close
     end
+
 
     def establish_connection
       @connection = Bunny.new(
@@ -29,11 +45,12 @@ module AmqpManager
       ).tap { |c| c.start }
     end
 
+
     def start
       establish_connection
 
-      queue.bind(xchange, routing_key: 'voice.rails')
-      queue.subscribe do |delivery_info, metadata, payload|
+      rails_queue.bind(rails_xchange, routing_key: 'voice.rails')
+      rails_queue.subscribe do |delivery_info, metadata, payload|
         # ...
       end
     end
