febe00acbdbe61b8823bb35b09bc2cc66863b319
Change Me <change@me.com>
2014-07-01 14:16:47 +0200

    first green rails spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+
+  it "cereate an instance of its klass" do
+    expect(User.new).to be_a User
+  end
 end
