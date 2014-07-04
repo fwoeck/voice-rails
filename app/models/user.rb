6925f9195cc7425a45225916c44ff79311ba3408
Frank Wöckener <fwoeck@gmail.com>
2014-07-04 11:17:17 +0200

    add basic user availability storage and signaling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+
+  # The "type" field is being used by asterisk for friend/peer:
   self.inheritance_column = :_type_disabled
 
   devise :database_authenticatable, :registerable,
@@ -7,6 +9,18 @@ class User < ActiveRecord::Base
   has_many :roles
   has_many :skills
   has_many :languages
+
+
+  def set_availability(key)
+    check_for_validity(:availability, key)
+    $redis.set(availability_keyname, key, ex: availability_ttl)
+    notify_ahn_about_update(:availability)
+  end
+
+  def availability
+    $redis.get(availability_keyname) || 'unknown'
+  end
+  alias :availability_summary :availability
 
 
   def set_language(key)
@@ -69,14 +83,27 @@ class User < ActiveRecord::Base
 
 
   def check_for_validity(field, key)
-    unless WimConfig.send("#{field}s").keys.include?(key)
-      raise "Invalid user #{field}. Use any of #{WimConfig.send("#{field}s").keys.join(',')}"
+    keys = WimConfig.send("#{field}#{field[/y\z/] ? '' : 's'}").keys
+    unless keys.include?(key)
+      raise "Invalid user #{field} - use any of #{keys.join(',')}"
     end
   end
 
 
   def notify_ahn_about_update(key)
-    reload
-    AmqpManager.ahn_publish user_id: self.id, key => send("#{key}_summary")
+    self.reload
+    AmqpManager.ahn_publish({
+      user_id: self.id,
+      key   => send("#{key}_summary")
+    })
+  end
+
+
+  def availability_keyname
+    "#{Rails.env}.availability.#{self.id}"
+  end
+
+  def availability_ttl
+    1.day
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,40 +4,67 @@ RSpec.describe User, type: :model do
 
   let(:user) { create(:user) }
 
-  it 'allows to set a user language' do
-    user.set_language('it')
-    expect(user.reload.languages.map(&:name)).to eq ['it']
+
+  context 'setting user attributes' do
+
+    it 'allows to set a user language' do
+      user.set_language('it')
+      expect(user.reload.languages.map(&:name)).to eq ['it']
+    end
+
+    it 'ignores setting a user language twice' do
+      user.set_language('it')
+      user.set_language('it')
+      expect(user.reload.languages.map(&:name)).to eq ['it']
+    end
+
+    it 'allows to set multiple languages' do
+      user.set_language('it')
+      user.set_language('es')
+      expect(user.reload.languages.map(&:name)).to eq ['it', 'es']
+    end
+
+    it 'allows to remove a language' do
+      user.set_language('it')
+      user.set_language('es')
+      user.unset_language('it')
+      expect(user.reload.languages.map(&:name)).to eq ['es']
+    end
+
+    it 'ignores unsetting an unset language' do
+      user.unset_language('it')
+      expect(user.reload.languages.map(&:name)).to eq []
+    end
+
+    it 'checks the language argument validity on set' do
+      expect { user.set_language('none') }. to raise_error
+    end
+
+    it 'checks the language argument validity on unset' do
+      expect { user.unset_language('none') }. to raise_error
+    end
   end
 
-  it 'ignores setting a user language twice' do
-    user.set_language('it')
-    user.set_language('it')
-    expect(user.reload.languages.map(&:name)).to eq ['it']
-  end
 
-  it 'allows to set multiple languages' do
-    user.set_language('it')
-    user.set_language('es')
-    expect(user.reload.languages.map(&:name)).to eq ['it', 'es']
-  end
+  context 'setting the user availability' do
 
-  it 'allows to remove a language' do
-    user.set_language('it')
-    user.set_language('es')
-    user.unset_language('it')
-    expect(user.reload.languages.map(&:name)).to eq ['es']
-  end
+    it 'allows to set the user availability' do
+      user.set_availability('busy')
+      expect(user.availability).to eq 'busy'
+    end
 
-  it 'ignores unsetting an unset language' do
-    user.unset_language('it')
-    expect(user.reload.languages.map(&:name)).to eq []
-  end
+    it 'allows to reset the user availability' do
+      user.set_availability('busy')
+      user.set_availability('offline')
+      expect(user.availability).to eq 'offline'
+    end
 
-  it 'checks the language argument validity on set' do
-    expect { user.set_language('none') }. to raise_error
-  end
+    it 'shows an unknown state if unset' do
+      expect(user.availability).to eq 'unknown'
+    end
 
-  it 'checks the language argument validity on unset' do
-    expect { user.unset_language('none') }. to raise_error
+    it 'checks the availability argument validity on set' do
+      expect { user.set_availability('none') }. to raise_error
+    end
   end
 end
