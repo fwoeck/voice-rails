d48ac86a21567006f17b0c09cfb7223b429ae634
Frank Wöckener <fwoeck@gmail.com>
2014-07-06 08:41:30 +0200

    add some login form styling

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-WimConfig = YAML.load_file('./config/app.yml')
+WimVersion = `git tag | sort | tail -1`.strip
+WimConfig  = YAML.load_file('./config/app.yml')
+
 WimConfig.keys.each { |key|
   WimConfig.instance_eval "class << self; define_method(:#{key}) {self['#{key}']}; end"
 }
