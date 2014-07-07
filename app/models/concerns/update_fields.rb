87626da43c8f85db7da6824c39c4717fda2acee8
Frank Wöckener <fwoeck@gmail.com>
2014-07-07 15:57:47 +0200

    move some user methods to a concern

---
+++ app/models/concerns/update_fields.rb
@@ -0,0 +1,126 @@
+module UpdateFields
+  extend ActiveSupport::Concern
+
+
+  def set_availability(key)
+    check_for_validity(:availability, key)
+    $redis.set(availability_keyname, key, ex: availability_ttl)
+    notify_ahn_about_update(:availability)
+  end
+
+  def availability
+    $redis.get(availability_keyname) || 'unknown'
+  end
+  alias :availability_summary :availability
+
+
+  def set_language(key)
+    set_field(:language, key)
+  end
+
+  def unset_language(key)
+    unset_field(:language, key)
+  end
+
+
+  def set_role(key)
+    set_field(:role, key)
+  end
+
+  def unset_role(key)
+    unset_field(:role, key)
+  end
+
+
+  def set_skill(key)
+    set_field(:skill, key)
+  end
+
+  def unset_skill(key)
+    unset_field(:skill, key)
+  end
+
+
+  def role_summary
+    roles.map(&:name).join(',')
+  end
+
+  def skill_summary
+    skills.map(&:name).join(',')
+  end
+
+  def language_summary
+    languages.map(&:name).join(',')
+  end
+
+  private
+
+
+  def set_field(field, _key)
+    key = _key.to_s
+    check_for_validity(field, key)
+    self.send("#{field}s").find_or_create_by(name: key)
+    notify_ahn_about_update(field)
+  end
+
+
+  def unset_field(field, _key)
+    key = _key.to_s
+    check_for_validity(field, key)
+    self.send("#{field}s").find_by(name: key).try(:delete)
+    notify_ahn_about_update(field)
+  end
+
+
+  def check_for_validity(field, key)
+    keys = WimConfig.send("#{field}#{field[/y\z/] ? '' : 's'}").keys
+    unless keys.include?(key)
+      raise "Invalid user #{field} - use any of #{keys.join(',')}"
+    end
+  end
+
+
+  def availability_keyname
+    "#{Rails.env}.availability.#{self.id}"
+  end
+
+  def availability_ttl
+    1.day
+  end
+
+
+  def update_availability_from(params)
+    _availability = params[:user].fetch(:availability, availability)
+    if _availability != availability
+      set_availability(_availability)
+    end
+  end
+
+
+  def update_languages_from(params)
+    old_langs = languages.map(&:name).sort
+    new_langs = params[:user].fetch(:languages).split(',').sort
+
+    if old_langs != new_langs
+      (old_langs - new_langs).each { |l| unset_language(l) }
+      (new_langs - old_langs).each { |l| set_language(l) }
+    end
+  end
+
+
+  def update_skills_from(params)
+    old_skills = skills.map(&:name).sort
+    new_skills = params[:user].fetch(:skills).split(',').sort
+
+    if old_skills != new_skills
+      (old_skills - new_skills).each { |l| unset_skill(l) }
+      (new_skills - old_skills).each { |l| set_skill(l) }
+    end
+  end
+
+
+  def update_fields_from(params)
+    self.fullname = params[:user].fetch(:fullname, fullname)
+    save!
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
   has_many :skills
   has_many :languages
 
+  include UpdateFields
+
 
   def update_attributes_from(params)
     update_availability_from(params)
@@ -27,84 +29,7 @@ class User < ActiveRecord::Base
     end
   end
 
-
-  def set_availability(key)
-    check_for_validity(:availability, key)
-    $redis.set(availability_keyname, key, ex: availability_ttl)
-    notify_ahn_about_update(:availability)
-  end
-
-  def availability
-    $redis.get(availability_keyname) || 'unknown'
-  end
-  alias :availability_summary :availability
-
-
-  def set_language(key)
-    set_field(:language, key)
-  end
-
-  def unset_language(key)
-    unset_field(:language, key)
-  end
-
-
-  def set_role(key)
-    set_field(:role, key)
-  end
-
-  def unset_role(key)
-    unset_field(:role, key)
-  end
-
-
-  def set_skill(key)
-    set_field(:skill, key)
-  end
-
-  def unset_skill(key)
-    unset_field(:skill, key)
-  end
-
-
-  def role_summary
-    roles.map(&:name).join(',')
-  end
-
-  def skill_summary
-    skills.map(&:name).join(',')
-  end
-
-  def language_summary
-    languages.map(&:name).join(',')
-  end
-
-
   private
-
-
-  def set_field(field, _key)
-    key = _key.to_s
-    check_for_validity(field, key)
-    self.send("#{field}s").find_or_create_by(name: key)
-    notify_ahn_about_update(field)
-  end
-
-
-  def unset_field(field, _key)
-    key = _key.to_s
-    check_for_validity(field, key)
-    self.send("#{field}s").find_by(name: key).try(:delete)
-    notify_ahn_about_update(field)
-  end
-
-
-  def check_for_validity(field, key)
-    keys = WimConfig.send("#{field}#{field[/y\z/] ? '' : 's'}").keys
-    unless keys.include?(key)
-      raise "Invalid user #{field} - use any of #{keys.join(',')}"
-    end
-  end
 
 
   def notify_ahn_about_update(key)
@@ -113,50 +38,5 @@ class User < ActiveRecord::Base
       user_id: self.id,
       key   => send("#{key}_summary")
     })
-  end
-
-
-  def availability_keyname
-    "#{Rails.env}.availability.#{self.id}"
-  end
-
-  def availability_ttl
-    1.day
-  end
-
-
-  def update_availability_from(params)
-    _availability = params[:user].fetch(:availability, availability)
-    if _availability != availability
-      set_availability(_availability)
-    end
-  end
-
-
-  def update_languages_from(params)
-    old_langs = languages.map(&:name).sort
-    new_langs = params[:user].fetch(:languages).split(',').sort
-
-    if old_langs != new_langs
-      (old_langs - new_langs).each { |l| unset_language(l) }
-      (new_langs - old_langs).each { |l| set_language(l) }
-    end
-  end
-
-
-  def update_skills_from(params)
-    old_skills = skills.map(&:name).sort
-    new_skills = params[:user].fetch(:skills).split(',').sort
-
-    if old_skills != new_skills
-      (old_skills - new_skills).each { |l| unset_skill(l) }
-      (new_skills - old_skills).each { |l| set_skill(l) }
-    end
-  end
-
-
-  def update_fields_from(params)
-    self.fullname = params[:user].fetch(:fullname, fullname)
-    save!
   end
 end
