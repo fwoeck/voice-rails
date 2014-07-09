359c6a1deae90d9c80329232c07f52d701ceae26
Frank Wöckener <fwoeck@gmail.com>
2014-07-09 11:20:47 +0200

    memo redis fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,13 +30,13 @@ class User < ActiveRecord::Base
   end
 
   def availability
-    $redis.get(availability_keyname) || 'unknown'
+    @memo_availability ||= ($redis.get(availability_keyname) || 'unknown')
   end
   alias :availability_summary :availability
 
 
   def agent_state
-    $redis.get(agent_state_keyname) || 'unknown'
+    @memo_agent_state ||= ($redis.get(agent_state_keyname) || 'unknown')
   end
 
 
