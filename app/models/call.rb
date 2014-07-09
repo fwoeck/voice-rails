1e679c287040da37a6cc590c1f8e7511daa89b2b
Frank Wöckener <fwoeck@gmail.com>
2014-07-09 14:43:27 +0200

    add caller_id to call object

--- app/models/call.rb
+++ app/models/call.rb
@@ -3,21 +3,24 @@ require 'json'
 class Call
   include ActiveModel::Serialization
 
-  attr_accessor :channel1, :channel2, :target_id, :language, :skill, :hungup
+  attr_accessor :channel1, :channel2, :target_id,
+                :language, :skill, :hungup, :caller_id
 
-
-  def initialize(tcid=nil, chan1=nil, chan2=nil, lang=nil, skill=nil, hungup=nil)
+  # FIXME Refactor n-arity into parameter object:
+  #
+  def initialize(tcid=nil, chan1=nil, chan2=nil, lang=nil, skill=nil, hungup=nil, cid=nil)
     @target_id = tcid;  @hungup   = hungup
     @channel1  = chan1; @channel2 = chan2
     @language  = lang;  @skill    = skill
+    @caller_id = cid
   end
 
 
   def headers
     {
-      'Channel1' => channel1, 'Channel2' => channel2,
-      'Language' => language, 'Skill'    => skill,
-      'Hungup'   => hungup
+      'Channel1' => channel1,  'Channel2' => channel2,
+      'Language' => language,  'Skill'    => skill,
+      'CallerId' => caller_id, 'Hungup'   => hungup
     }
   end
 
@@ -39,7 +42,7 @@ class Call
     new(tcid,
       fields['Channel1'], fields['Channel2'],
       fields['Language'], fields['Skill'],
-      fields['Hungup']
+      fields['Hungup'], fields['CallerId']
     )
   end
 
--- app/serializers/call_serializer.rb
+++ app/serializers/call_serializer.rb
@@ -1,6 +1,6 @@
 class CallSerializer < ActiveModel::Serializer
 
-  attributes :channel1, :channel2, :id, :language, :skill, :hungup
+  attributes :channel1, :channel2, :id, :language, :skill, :hungup, :caller_id
 
   def id
     object.target_id
