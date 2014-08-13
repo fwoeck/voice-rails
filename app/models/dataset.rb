fd62429ebb05e9d707356f0e96f7b6afeccef98e
Frank Wöckener <fwoeck@gmail.com>
2014-08-13 11:19:00 +0200

    move call dataset aggregation to VN

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -3,67 +3,42 @@ require 'json'
 class Dataset
 
   include ActiveModel::Serialization
+  attr_reader :dataset
+
 
   def id; 1; end
 
-
   def initialize
+    @dataset = get_dataset
   end
 
 
   def active_call_count
-    pre_queued_call_count + queued_call_count + dispatched_call_count
+    dataset.fetch('active_call_count', 0)
   end
 
 
   def pre_queued_call_count
-    pre_queued_calls.size
+    dataset.fetch('pre_queued_call_count', 0)
   end
 
 
   def queued_call_count
-    queued_calls.size
+    dataset.fetch('queued_call_count', 0)
   end
 
 
   def dispatched_call_count
-    dispatched_call_pairs.keys.size
+    dataset.fetch('dispatched_call_count', 0)
   end
 
 
-  def raw_calls
-    @memo_raw_calls ||= $redis.keys("#{Rails.env}.call.*").map { |k|
-      JSON.parse $redis.get(k) || '{}'
-    }
-  end
+  private
 
-
-  def pre_queued_calls
-    incoming_calls.select { |c| !c['Skill'] && !c['DispatchedAt'] }
-  end
-
-
-  def queued_calls
-    incoming_calls.select { |c| c['Skill'] && !c['DispatchedAt'] }
-  end
-
-
-  def incoming_calls
-    raw_calls.select { |c|
-      !c['CallTag'] && !c['Hungup'] && (
-        c['Extension'].blank? || c['Extension'] == '100'
-      )
-    }
-  end
-
-
-  def dispatched_calls
-    raw_calls.select { |c| c['CallTag'] && !c['Hungup'] }
-  end
-
-
-  def dispatched_call_pairs
-    dispatched_calls.group_by { |c| c['CallTag'] }
+  def get_dataset
+    JSON.parse(
+      $redis.get("#{Rails.env}.numbers-dataset") || '{}'
+    )
   end
 
 
