6068d05c9718d5d7a72d88e3685cb57b542d663c
Frank Wöckener <fwoeck@gmail.com>
2014-09-23 12:01:22 +0200

    try to conserve AR connections

--- app/models/call.rb
+++ app/models/call.rb
@@ -48,13 +48,15 @@ class Call
 
 
   def handle_message
-    uids = if hide_from_agents?
-      User.all_online_ids & User.all_admin_ids
-    else
-      User.all_online_ids
-    end
+    ActiveRecord::Base.connection_pool.with_connection {
+      uids = if hide_from_agents?
+        User.all_online_ids & User.all_admin_ids
+      else
+        User.all_online_ids
+      end
 
-    send_call_update_to_clients(uids)
+      send_call_update_to_clients(uids)
+    }
   end
 
 
